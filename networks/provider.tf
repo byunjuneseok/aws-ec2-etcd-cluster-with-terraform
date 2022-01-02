1f5e15ec2cb5a00bdecba52ecdbb22e2d484ff1b
byunjuneseok <byunjuneseok@gmail.com>
2022-01-02 13:34:13 +0900

    Add missing required_providers

--- networks/provider.tf
+++ networks/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.70.0"
+    }
+  }
+}
+
 provider "aws" {
   profile = "${var.aws_profile}"
   region  = "${var.cluster_region}"
